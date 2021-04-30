40b25a779f9c050b420358235e6d55a955fb1547
Dmitry Gornak <lonnyfox@bk.ru>
2021-05-01 01:22:37 +0300

    item gallery

--- src/ui/Container/Container.tsx
+++ src/ui/Container/Container.tsx
@@ -3,4 +3,5 @@ export const Container = styled.div`
     width: 100%;
     max-width: 1200px;
     padding: 0px 20px;
+    margin: auto;
 `
--- src/ui/ItemGalleryUI/ItemGalleryUI.tsx
+++ src/ui/ItemGalleryUI/ItemGalleryUI.tsx
@@ -1,4 +1,55 @@
+import { useState } from 'react';
+import ImageGallery from "react-image-gallery";
+import ReactImageMagnify from "react-image-magnify";
+import "react-image-gallery/styles/css/image-gallery.css";
+import styled from 'styled-components';
 export const ItemGalleryUI = (props: any) => {
     const { images } = props;
-    return <p>image gallery</p>
+    const [isFullScreen, setIsFullScreen] = useState<boolean>(false);
+    return (
+        <GalleryWrapper>
+            <ImageGallery
+                thumbnailPosition={"bottom"}
+                useBrowserFullscreen={false}
+                showPlayButton={false}
+                onScreenChange={() => setIsFullScreen(!isFullScreen)}
+                renderItem={isFullScreen ? undefined : (item) => {
+                    return (
+                        <ReactImageMagnify
+                            {...{
+                                smallImage: {
+                                    // alt: "Wristwatch by Ted Baker London",
+                                    isFluidWidth: true,
+                                    src: item?.original
+                                },
+                                largeImage: {
+                                    src: item?.original,
+                                    width: 640,
+                                    height: 480
+                                },
+                                enlargedImagePortalId: "galleryPortal"
+                            }}
+                        />
+                    )
+                }}
+                items={images.map((img: string) => {
+                    return {
+                        original: img,
+                        thumbnail: img
+                    }
+                })}
+            />
+            <div id="galleryPortal"></div>
+        </GalleryWrapper>
+    )
 }
+
+const GalleryWrapper = styled.div`
+    max-width: 412px;
+    position: relative;
+    & #galleryPortal {
+        position: absolute;
+        left: 100%;
+        top: 0;
+    }
+`
